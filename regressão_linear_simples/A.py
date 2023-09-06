48ceadffaef21c4c7321e8a5d29886330fe8fcbe
Luciano Ramos <thalmenoch@gmail.com>
2023-09-06 18:58:40 -0300

    ajustes no A

--- regressão_linear_simples/A.py
+++ regressão_linear_simples/A.py
@@ -1,7 +1,17 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from functions import *
+
+#Cálculo do MSE
+def MSE(y, y_previsto):
+    return np.mean((y - y_previsto) ** 2)
+
+#Cálculo dos coeficientes
+def coef_linear_angular(x, y, media_x, media_y):
+    coef_angular = np.sum((x - media_x) * (y - media_y)) / np.sum((x - media_x) ** 2)
+    coef_linear = media_y - coef_angular * media_x
+
+    return coef_angular, coef_linear 
 
 if __name__ == '__main__':
 
